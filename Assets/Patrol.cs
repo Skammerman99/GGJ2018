fdb1a03af27eef55bf7cde75d3df14dcd6b0afd1
Spencer Kammerman <specialksocray@gmail.com>
2018-01-27 14:32:01 -0800

    Patrol stuff

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -6,7 +6,7 @@ public class Patrol : MonoBehaviour {
 
     private Vector3 pos1 = new Vector3(-4, 0, 0);
     private Vector3 pos2 = new Vector3(4, 0, 0);
-    public float speed = 0.01f;
+    public float speed = 0.1f;
     float timer = 0f;
 
     // Use this for initialization
