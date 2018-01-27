88d5d8006deef84ed60b675a33c2c471d612c5cd
Spencer Kammerman <specialksocray@gmail.com>
2018-01-27 11:01:38 -0800

    Added base of patrol code
    For patrolling enemies and guards

---
+++ Assets/Patrol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+
+    private Vector3 pos1 = new Vector3(-4, 0, 0);
+    private Vector3 pos2 = new Vector3(4, 0, 0);
+    public float speed = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time/3 * speed, 1.0f));
+    }
+}
