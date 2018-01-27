ac1db5f0ccf1119a36a6f16bf52c85260c573465
Spencer Kammerman <specialksocray@gmail.com>
2018-01-27 12:52:21 -0800

    Enemy that patrols and turns with vision
    Hitboxes need balancing/adjustment

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -7,6 +7,7 @@ public class Patrol : MonoBehaviour {
     private Vector3 pos1 = new Vector3(-4, 0, 0);
     private Vector3 pos2 = new Vector3(4, 0, 0);
     public float speed = 0.01f;
+    float timer = 0f;
 
     // Use this for initialization
     void Start () {
@@ -16,5 +17,13 @@ public class Patrol : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time/3 * speed, 1.0f));
+
+        timer += Time.deltaTime;
+
+        if (timer >= 3)
+        {
+            timer = 0;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
     }
 }
