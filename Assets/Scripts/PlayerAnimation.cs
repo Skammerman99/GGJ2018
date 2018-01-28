1d1681fc1d875505a0ffde3634a8d80eda05bf3a
Blong Timothy Yang <blongty@uci.edu>
2018-01-28 01:16:57 -0800

    Player Animations
    Just for Stage 2-2 right now

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    public Sprite[] sprites;
+    public float fps;
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int index = (int)(Time.timeSinceLevelLoad * fps);
+        index = index % sprites.Length - 1;
+        spriteRenderer.sprite = sprites[index];
+	}
+}
