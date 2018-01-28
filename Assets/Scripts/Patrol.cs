d85ffe1cdb5cb4c9a0cff5aa576b750d2ef8eb49
Spencer Kammerman <specialksocray@gmail.com>
2018-01-27 16:09:30 -0800

    MORE PATROL LOGIC WEEEEE

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour {
 
-    private Vector3 pos1 = new Vector3(-4, 0, 0);
-    private Vector3 pos2 = new Vector3(4, 0, 0);
+    public GameObject obj;
+
+    private Vector3 pos1;
+    private Vector3 pos2;
     public float speed = 0.1f;
     float timer = 0f;
 
     // Use this for initialization
     void Start () {
-		
-	}
+
+           pos1 = new Vector3(-4, obj.transform.position.y, obj.transform.position.z);
+              pos2 = new Vector3(4, obj.transform.position.y, obj.transform.position.z);
+}
 	
 	// Update is called once per frame
 	void Update () {
