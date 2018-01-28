9d54e1d9891b997abd169252bf0d9b305841aa9d
Blong Timothy Yang <blongty@uci.edu>
2018-01-27 19:09:45 -0800

    Camera and Texture Update, Movement
    Title

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -2,30 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-    public  float jumpHeight;
-    public  float movement;
+    public float jumpHeight;
+    public float movement;
     public Transform lvl_ZeroCheck;
     public float lvl_zeroRad;
     public LayerMask lvl_ZeroDef;
     private bool lvl_Zero;
+    public Transform wall_check_right;
+    public Transform wall_check_left;
+    public float wall_rad;
+    public LayerMask wallDef;
+    public bool wall_left;
+    public bool wall_right;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         GetComponent<Rigidbody2D>().freezeRotation = true;
-        
-		
-	}
-	
-	// Update is called once per frame
+
+
+    }
+
+    // Update is called once per frame
     void FixedUpdate()
     {
         lvl_Zero = Physics2D.OverlapCircle(lvl_ZeroCheck.position, lvl_zeroRad, lvl_ZeroDef);
+        wall_right = Physics2D.OverlapCircle(wall_check_right.position, wall_rad, wallDef);
+        wall_left = Physics2D.OverlapCircle(wall_check_left.position, wall_rad, wallDef);
+
     }
-	void Update () {
-//jumping
-        if(Input.GetKeyDown (KeyCode.Space) && lvl_Zero)
+    void Update()
+    {
+        //jumping
+        if (Input.GetKeyDown(KeyCode.Space) && lvl_Zero)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
         }
@@ -38,20 +51,20 @@ public class PlayerMovement : MonoBehaviour {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
         }
         //moving forward
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && !wall_right)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(movement, GetComponent<Rigidbody2D>().velocity.y);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && !wall_right)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(movement, GetComponent<Rigidbody2D>().velocity.y);
         }
         //moving backward
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && !wall_left)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-movement, GetComponent<Rigidbody2D>().velocity.y);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && !wall_left)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-movement, GetComponent<Rigidbody2D>().velocity.y);
         }
