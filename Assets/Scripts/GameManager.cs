097129ea435b9295acd6647a1ceab3481e0afff0
Unknown <calebsc1@uci.edu>
2018-01-28 10:33:39 -0800

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using UnityEngine.UI;
+
 public class GameManager : MonoBehaviour {
 
+    public Image black;//IMage
+    public Animator anim;
 
     public IEnumerator Fadery()
     {
         float fadeTime = GameObject.Find("GameManager").GetComponent<Fading>().BeginFade(1);
         yield return new WaitForSeconds(fadeTime);
         
+    }//Get rid of
+
+        
+
+    IEnumerator Fadding()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
     }
+
+
 
     public void SceneLoader(int SceneIndex)
      {
-
+       StartCoroutine(Fadding());
        Fadery();
        SceneManager.LoadScene(SceneIndex + 1);
     }
