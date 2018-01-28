8dc1d258f11ad2933e56fdc824b03ab3a889e4bd
Blong Timothy Yang <blongty@uci.edu>
2018-01-28 15:00:36 -0800

    BGM Added, Player Prefab modified
    Required due to BGM additions

---
+++ Assets/BGMScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMScript : MonoBehaviour {
+
+    public AudioSource Source;
+    public AudioClip BGM;
+     
+
+	// Use this for initialization
+	void Start () {
+        Source.PlayOneShot(BGM, 0.5F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
